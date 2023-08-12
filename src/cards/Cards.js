a7bdd22149c6726295ee7e0f44c330e235586c72
Rabee Aljaramani <rabeejermani@gmail.com>
2023-08-12 21:21:32 +0400

    filtering

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../card/Card';
 import { data } from '../data';
+import FiltersForm from '../filters_form/FiltersForm';
 export default function Cards() {
+  const [searching, setSearching] = useState(false)
+  const [listToBeRendered, setListToBeRendered] = useState(data)
+  const original_list = data
   return (
     <div className="cards-cont">
+      <FiltersForm original_list={original_list} setListToBeRendered={setListToBeRendered} listToBeRendered={listToBeRendered} searching={searching} setSearching={setSearching} />
       <div className="cards-grid">
-        {data.map((item) => {
+        {listToBeRendered.map((item) => {
           return <Card item={item} key={item.requisition_number} />;
         })}
       </div>
---
+++ src/filters_form/FiltersForm.js
@@ -0,0 +1,136 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from '@mui/material';
+
+// import { makeStyles } from '@mui/styles'
+export default function FiltersForm(props) {
+
+  // const useStyles = makeStyles({
+  //   paper: {
+  //     overflowY: "scroll",
+  //     height: "450px"
+  //   }
+  // });
+  // const classes = useStyles();
+  // const MenuProps = {
+  //   autoFocus: false
+  // };
+  // get the list and the setlist from props
+  const searching = props.searching
+  const setSearching = props.setSearching
+  const setListToBeRendered = props.setListToBeRendered
+  const listToBeRendered = props.listToBeRendered
+  const original_list = props.original_list
+  const [country, setCountry] = useState('all');
+  const [job_title, setJob_title] = useState('all');
+  const [job_title_list, setJob_title_list] = useState(listToBeRendered)
+
+  const filterByCountry = (listToBeFiltered) => {
+    console.log('listToBeFiltered', listToBeFiltered)
+
+    if (country !== 'all') {
+      let temp_list = listToBeFiltered.filter((e) => {
+        console.log(e.location)
+        return e.location.toLowerCase().includes(country.toLowerCase());
+      });
+      return filterByJobTitle(temp_list);
+      // return temp_list;
+    } else {
+      return filterByJobTitle(listToBeFiltered);
+      // return listToBeFiltered;
+    }
+  };
+
+  const filterByJobTitle = (listToBeFiltered) => {
+    if (job_title !== 'all') {
+      let temp_list = listToBeFiltered.filter((e) => {
+        return e.job_title.toLowerCase() === job_title.toLowerCase();
+      });
+      setSearching(false)
+      return temp_list;
+    } else {
+      setSearching(false)
+      return listToBeFiltered;
+    }
+  };
+  const setListToRender = (list) => {
+    setListToBeRendered(filterByCountry(list))
+  }
+
+  const handleSubmit = () => {
+    setSearching(true);
+    setTimeout(() => {
+      setListToRender(original_list);
+    }, 1000);
+  };
+  function removeDuplicates(arr) {
+    return arr.filter((item, index) => arr.indexOf(item) === index);
+  }
+  useEffect(() => {
+    setJob_title_list(removeDuplicates(listToBeRendered))
+  }, [listToBeRendered])
+
+  return (
+    <>
+      <div className="filters-form-cont">
+
+        <FormControl fullWidth>
+          <InputLabel>BY LOCATION</InputLabel>
+          <Select
+            value={country}
+            label="BY COUNTRY"
+            onChange={(e) => setCountry(e.target.value)}
+            disabled={searching ? true : false}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="UAE">UAE</MenuItem>
+            <MenuItem value="QATAR">QATAR</MenuItem>
+            <MenuItem value="KSA">KSA</MenuItem>
+            <MenuItem value="KUWAIT">KUWAIT</MenuItem>
+            <MenuItem value="OMAN">OMAN</MenuItem>
+            <MenuItem value="BAHRAIN">BAHRAIN</MenuItem>
+            <MenuItem value="BAHRAIN">INDIA</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth>
+          <InputLabel>BY JOB TITLE</InputLabel>
+
+          <Select
+            value={job_title}
+            label="BY ROLE TYPE"
+            onChange={(e) => setJob_title(e.target.value)}
+            disabled={searching ? true : false}
+          >
+            <MenuItem value="all">All</MenuItem>
+            {job_title_list.map((e) => {
+              return <MenuItem key={e.requisition_number} value={e.job_title}>{e.job_title}</MenuItem>
+            })}
+            {/* <MenuItem value="leadership">Leadership</MenuItem>
+            <MenuItem value="sales front office">Sales Front Office</MenuItem>
+            <MenuItem value="sales back office">Sales Back Office</MenuItem>
+            <MenuItem value="support function">Support Function</MenuItem>
+            <MenuItem value="distribution centre">Distribution Centre</MenuItem> */}
+          </Select>
+        </FormControl>
+
+
+
+
+
+        <Button
+          disabled={searching ? true : false}
+          onClick={handleSubmit}
+          variant="contained"
+        >
+          SEARCH
+        </Button>
+      </div>
+    </>
+  );
+}
